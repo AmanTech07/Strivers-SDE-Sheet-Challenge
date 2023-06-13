30b058ce1edfc2a09f5d05dcf2eb6e4bcc09b6e3
Aman Yadav <87381439+AmanTech07@users.noreply.github.com>
2023-06-13 13:27:34 +0530

    Time: 104 ms (75.21%), Space: 28.3 MB (70.87%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& v) {
         int n = v.size(), cnt = 0;
-        for(int p=0; p<n; p++) {
-            for(int k=0; k<n; k++) {
-                int i=0, j=0;
-                while(i<n && j<n && v[i][k]==v[p][j]) {
-                    i++, j++;
-                }
-                if(i>=n) cnt++;
+        map<vector<int>,int> m;
+        for(int i=0; i<n; i++) {
+            m[v[i]] += 1;
+        }
+        for(int i=0; i<n; i++) {
+            vector<int> vec(n);
+            for(int j=0; j<n; j++) {
+                vec[j] = v[j][i];
             }
+            cnt += m[vec];
         }
         return cnt;
     }
