7e7d3113c2ac2a504f793ad7e7216f1c5a867285
Aman Yadav <87381439+AmanTech07@users.noreply.github.com>
2023-06-15 00:49:16 +0530

    Time: 49 ms (73.35%), Space: 14.5 MB (63.16%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,36 +9,58 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int l1 = 0, l2 = 0;
-        ListNode *ptr1 = headA, *ptr2 = headB;
-        while(ptr1) {
-            ++l1;
-            ptr1 = ptr1->next;
+        // int l1 = 0, l2 = 0;
+        // ListNode *ptr1 = headA, *ptr2 = headB;
+        // while(ptr1) {
+        //     ++l1;
+        //     ptr1 = ptr1->next;
+        // }
+        // while(ptr2) {
+        //     ++l2;
+        //     ptr2 = ptr2->next;
+        // }
+        // ptr1 = headA, ptr2 = headB;
+        // if(l1>l2) {
+        //     l1 = (l2-l1);
+        //     while(l1<0) {
+        //         ++l1;
+        //         ptr1 = ptr1->next;
+        //     }
+        // }
+        // else if(l2>l1) {
+        //     l2 = (l1-l2);
+        //     while(l2<0) {
+        //         ++l2;
+        //         ptr2 = ptr2->next;
+        //     }
+        // }
+        // while(ptr1 && ptr2) {
+        //     if(ptr1==ptr2) return ptr1;
+        //     ptr1 = ptr1->next;
+        //     ptr2 = ptr2->next;
+        // }
+        // return NULL;
+
+        ListNode* tail = headA, *slow = headB, *fast = headB;
+        while(tail->next) {
+            tail = tail->next;
         }
-        while(ptr2) {
-            ++l2;
-            ptr2 = ptr2->next;
-        }
-        ptr1 = headA, ptr2 = headB;
-        if(l1>l2) {
-            l1 = (l2-l1);
-            while(l1<0) {
-                ++l1;
-                ptr1 = ptr1->next;
+        tail->next = headA;
+
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast) {
+                slow = headB;
+                while(slow!=fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                tail->next = NULL;
+                return slow;
             }
         }
-        else if(l2>l1) {
-            l2 = (l1-l2);
-            while(l2<0) {
-                ++l2;
-                ptr2 = ptr2->next;
-            }
-        }
-        while(ptr1 && ptr2) {
-            if(ptr1==ptr2) return ptr1;
-            ptr1 = ptr1->next;
-            ptr2 = ptr2->next;
-        }
+        tail->next = NULL;
         return NULL;
     }
 };
