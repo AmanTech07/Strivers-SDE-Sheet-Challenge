f2c3786e29c8c00a075c70ff136e455f671ff1fd
Aman Yadav <87381439+AmanTech07@users.noreply.github.com>
2023-08-11 19:10:35 +0530

    Time: 48 ms (33.63%), Space: 18.6 MB (37.65%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    int func(int i, int amt, vector<int>& coins, vector<vector<int>>& dp) {
-        if(amt==0) return 1;
-        if(i==coins.size()) return 0;
-        if(dp[i][amt]!=-1) return dp[i][amt];
-        int ans = 0;
-        for(int j=i; j<coins.size(); j++) {
-            if(coins[j]<=amt) ans += func(j, amt-coins[j], coins, dp);
-        }
-        return dp[i][amt] = ans;
-    }
+    // int func(int i, int amt, vector<int>& coins, vector<vector<int>>& dp) {
+    //     if(amt==0) return 1;
+    //     if(i==-1) return 0;
+    //     if(dp[i][amt]!=-1) return dp[i][amt];
+    //     int ans = func(i-1, amt, coins, dp);
+    //     if(coins[i]<=amt) ans += func(i, amt-coins[i], coins, dp);
+    //     return dp[i][amt] = ans;
+    // }
 
     int change(int amt, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size()+1, vector<int>(amt+1, -1));
-        return func(0, amt, coins, dp);
+        vector<vector<int>> dp(coins.size()+1, vector<int>(amt+1, 0));
+        // return func(coins.size()-1, amt, coins, dp);
+        int n = coins.size();
+        for(int i=0; i<n; i++) {
+            dp[i][0] = 1;
+        }
+        for(int i=1; i<=n; i++) {
+            for(int j=0; j<=amt; j++) {
+                dp[i][j] = dp[i-1][j];
+                if(j>=coins[i-1]) dp[i][j] += dp[i][j-coins[i-1]];
+            }
+        }
+        return dp[n][amt];
     }
 };
