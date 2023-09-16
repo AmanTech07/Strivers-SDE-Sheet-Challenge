cd4fd4fcdf49d22044332c98f7a671059debbaad
Aman Yadav <87381439+AmanTech07@users.noreply.github.com>
2023-09-16 11:58:01 +0530

    Time: 174 ms (33.06%), Space: 37.4 MB (21.5%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int minimumEffortPath(vector<vector<int>>& g) {
-        int n = g.size(), m = g[0].size();
-        vector<vector<int>> dif(n, vector<int> (m, 1e9));
-        dif[0][0] = 0;
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        pq.push({0, {0, 0}});
-        int x[] = {1, -1, 0, 0}, y[] = {0, 0, 1, -1};
-        while(!pq.empty()) {
-            auto ele = pq.top();
-            pq.pop();
-            int prev = ele.first, x_cur = ele.second.first, y_cur = ele.second.second;
-            if(x_cur==n-1 && y_cur==m-1) return dif[n-1][m-1];
-            for(int k=0; k<4; k++) {
-                int i = x[k] + x_cur, j = y[k] + y_cur;
-                if(i<0 || i==n || j<0 || j==m) continue;
-                int d = max(prev, abs(g[i][j]-g[x_cur][y_cur]));
-                if(dif[i][j]<=d) continue;
-                dif[i][j] = d;
-                pq.push({dif[i][j], {i, j}});
+    int minimumEffortPath(vector<vector<int>>& vec) {
+        int n = vec.size(), m = vec[0].size();
+        set<pair<int, pair<int, int>>> st;
+        vector<vector<int>> vis(n, vector<int>(m, 1e9));
+        st.insert({0, {0, 0}});
+        vis[0][0] = 0;
+        int x[4] = {0, 0, -1, 1}, y[4] = {-1, 1, 0, 0};
+        while(!st.empty()) {
+            auto it = *st.begin();
+            st.erase(it);
+            int effort = it.first, u = it.second.first, v = it.second.second;
+            for(int i=0; i<4; i++) {
+                int xx = u+x[i], yy = v+y[i];
+                if(xx>=0 && xx<n && yy>=0 && yy<m) {
+                    int prev = vec[u][v], nxt = vec[xx][yy];
+                    if(vis[xx][yy]>max(effort,abs(nxt-prev))) {
+                        vis[xx][yy] = max(abs(nxt-prev),effort);
+                        st.insert({vis[xx][yy], {xx, yy}});
+                    }
+                }
+                
             }
         }
-        return 0;
+        return vis[n-1][m-1];
     }
 };
