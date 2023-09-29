ff7cfb6076afa196b2ba1a1e47e5e8ba02b765d4
Aman Yadav <87381439+AmanTech07@users.noreply.github.com>
2023-09-29 13:52:18 +0530

    Time: 4 ms (23.26%), Space: 8.2 MB (6.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,12 @@
 class Solution {
-    int func(int i, vector<int> &v, vector<int> &dp) {
-        if(i>=v.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int ans = func(i+1, v, dp);
-        ans = max(ans, func(i+2, v, dp)+v[i]);
-        return dp[i] = ans;
-    }
 public:
     int rob(vector<int>& v) {
-        vector<int> dp(v.size()+1, -1);
-        return func(0, v, dp);
+        int n = v.size();
+        vector<int> dp(n+1, -1);
+        dp[0] = 0, dp[1] = v[0]; 
+        for(int i=2; i<=n; i++) {
+            dp[i] = max(dp[i-1], dp[i-2]+v[i-1]);
+        }
+        return dp[n];
     }
 };
