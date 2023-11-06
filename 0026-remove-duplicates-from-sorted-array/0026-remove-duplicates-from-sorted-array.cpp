2e88fe7767f1ac77b158408b5c94139f46d3e53a
Aman Yadav <87381439+AmanTech07@users.noreply.github.com>
2023-11-06 18:10:34 +0530

    Time: 7 ms (77.95%), Space: 18.8 MB (27.44%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,27 +1,11 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        // --------------------TC: O(n*log(n))-----------------
-        // --------------------SC: O(j)------------------------
-
-        // map<int, int> m;
-        // int n = nums.size(), j = 0;
-        // for(int i=0; i<n; i++) {
-        //     if(m.find(nums[i])==m.end()) {
-        //         m[nums[i]]++;
-        //         nums[j++] = nums[i];
-        //     }
-        // }
-        // return j;
-
-        // -------Since vector is sorted so we can do this is in O(n) TC and O(1) SC-------
-        // -------------------TC: O(n)-----------------
-        // -------------------SC: O(1)-----------------
-        int flg = nums[0], j=1, n = nums.size();
+    int removeDuplicates(vector<int>& v) {
+        int n = v.size(), prev = v[0], j = 1;
         for(int i=1; i<n; i++) {
-            if(nums[i]!=flg) {
-                nums[j++] = nums[i];
-                flg = nums[i];
+            if(v[i]!=prev) {
+                v[j++] = v[i];
+                prev = v[i];
             }
         }
         return j;
